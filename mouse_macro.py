4f583bb7ec83f898cae0233432e442aa0a7f9b6d
daexwaki <166275357+daexwaki@users.noreply.github.com>
2024-04-15 20:50:50 +0700

    Update mouse_macro.py

--- mouse_macro.py
+++ mouse_macro.py
@@ -16,21 +16,21 @@ class ScrollThread(threading.Thread):
                 time.sleep(0.01)
 
 def on_press(key):
-    global scroll_thread, is_x_pressed, is_ctrl_pressed
+    global scroll_thread, is_q_pressed, is_ctrl_pressed
     if key == keyboard.Key.esc:
         scroll_thread.is_scrolling = False
-    elif key == keyboard.KeyCode.from_char('x'):
+    elif key == keyboard.KeyCode.from_char('q'):
         is_x_pressed = True
     elif key == keyboard.Key.ctrl:
-        is_ctrl_pressed = True
+        is_ctrl_pressed = False
 
     # Check if both Ctrl and X are pressed simultaneously
-    if is_ctrl_pressed and is_x_pressed:
+    if is_ctrl_pressed and is_q_pressed:
         scroll_thread.is_scrolling = False
 
 def on_release(key):
-    global scroll_thread, is_x_pressed, is_ctrl_pressed
-    if key == keyboard.KeyCode.from_char('x'):
+    global scroll_thread, is_q_pressed, is_ctrl_pressed
+    if key == keyboard.KeyCode.from_char('q'):
         is_x_pressed = False
         if not is_ctrl_pressed:
             scroll_thread.is_scrolling = not scroll_thread.is_scrolling
